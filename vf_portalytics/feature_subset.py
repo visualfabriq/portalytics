c478a98357cecf8fbde6725350ba806573512ca9
ctselasvisualfabriq <ctselas@visualfabriq.com>
2020-02-10 11:41:05 +0100

    Adds a class transformer that takes data, excludes features that are used for grouping and perform every sklearn transformer. Adds a jupyter notebook that tests the transformer by itself and with a combination of the FeatureSubsetModel using pipeline and Cross Validation

--- vf_portalytics/feature_subset.py
+++ vf_portalytics/feature_subset.py
@@ -1,6 +1,30 @@
 import pandas as pd
 from copy import deepcopy
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
+
+
+class MyTransformer(TransformerMixin, BaseEstimator):
+
+    def __init__(self, group_cols=None, transformer=None):
+        """Build a feature tranformer"""
+        self.transformer = transformer
+        self.group_cols = group_cols
+
+    def fit(self, X, y=None):
+        """Drop the columns that are being used to group the data and fit the transformer"""
+        x_in = X.drop([n for n in self.group_cols], axis=1)
+        self.transformer = self.transformer.fit(X=x_in)
+        return self
+
+    def transform(self, X):
+        x_in = X.drop([n for n in self.group_cols], axis=1)
+        # transform the data
+        transformed_x = self.transformer.transform(X=x_in)
+        # convert data into initial format
+        transformed_x = pd.DataFrame(data=transformed_x, index=x_in.index,
+                                     columns=self.transformer.get_feature_names(x_in.columns))
+        transformed_x[list(self.group_cols)] = X[list(self.group_cols)]
+        return transformed_x
 
 
 class FeatureSubsetModel(BaseEstimator, RegressorMixin):
@@ -8,12 +32,13 @@ class FeatureSubsetModel(BaseEstimator, RegressorMixin):
     def __init__(self, group_cols=None, sub_models=None):
         """
         Build regression model for subsets of feature rows matching particular combination of feature columns.
+
         """
 
-        self.group_cols = deepcopy(group_cols) if group_cols else None
-        self.sub_models = deepcopy(sub_models) if sub_models else None
+        self.group_cols = group_cols
+        self.sub_models = sub_models
 
-    def fit(self, X, y):
+    def fit(self, X, y=None):
         """
         Partition the training data, X, into groups for each unique combination of values in
         ``self.group_cols`` columns. For each group, train the appropriate model specified in
@@ -30,15 +55,14 @@ class FeatureSubsetModel(BaseEstimator, RegressorMixin):
             x_in = x_group.drop([n for n in self.group_cols], axis=1)
             y_in = y.loc[x_in.index]
 
-            # Fit the sub-model with subset of rows
+            # Fit the submodel with subset of rows
             gp_model = gp_model.fit(X=x_in.values, y=y_in.values)
             self.sub_models[gp_key] = gp_model
-
         return self
 
-    def predict(self, X):
+    def predict(self, X, y=None):
         """
-        Same as ``self.fit()``, but call the ``predict()`` method for each sub-model and return the results.
+        Same as ``self.fit()``, but call the ``predict()`` method for each submodel and return the results.
         """
         groups = X.groupby(by=list(self.group_cols))
         results = []
