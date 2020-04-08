0ab0cbb26829c047fcefa03802bf9f57d075ccdb
ctselasvisualfabriq <ctselas@visualfabriq.com>
2020-04-08 16:15:03 +0200

    add grouped models, transformers and a jupyter example

---
+++ vf_portalytics/grouped_model.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import xgboost
+import copy
+
+from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
+from sklearn.dummy import DummyClassifier
+
+import models.transformers
+
+class Grouped_Model(BaseEstimator, RegressorMixin):
+
+    def __init__(self, group_col=None, clusters=None, params=None, selected_features=None):
+        """
+        Build a model for each subset of rows matching particular category of features group_col.
+        Input:
+            group_col: string; name of the column that the groups exist
+            clusters: array; with the name of unique groups
+            params: dictionary with keys the group names and values dictionaries of the selected hyperparameters
+        """
+        self.group_col = group_col
+        self.clusters = clusters
+        self.params = params
+        self.selected_features = selected_features
+        self.categorical_features = dict()
+        self.sub_models, self.transformers = self.initiliaze_models()
+
+    def fit(self, X, y=None):
+        """
+        Partition the training data, X, into groups for each unique combination of values in
+        'self.group_col' columns. For each group, train the appropriate model specified in 'self.sub_models'.
+        If there is no sub_model for a group, predict 0
+        The models are being trained using only the features that their names starts with 'promoted_price'
+        """
+        groups = X.groupby(by=self.group_col)
+        for gp_key, group in groups:
+            x_group = group[self.selected_features[gp_key]]
+            y_in = y.loc[x_group.index]
+            # preprocessing
+            self.categorical_features[gp_key] = Grouped_Model.set_categorical_features(data=x_group,
+                                                                                       potential_cat_feat=self.params[
+                                                                                           gp_key].get(
+                                                                                           'potential_cat_feat', None))
+            gp_transformer = self.transformers.get(gp_key)
+            gp_transformer.categorical_features = self.categorical_features[gp_key]
+            gp_transformer.fit(x_group)
+            x_group = gp_transformer.transform(x_group)
+
+            # Find the sub-model for this group key and fit
+            gp_model = self.sub_models.get(gp_key, DummyClassifier(constant=0))
+            gp_model = gp_model.fit(X=x_group, y=y_in.values)
+
+            self.sub_models[gp_key] = gp_model
+            self.transformers[gp_key] = gp_transformer
+            print('Model for ' + gp_key + ' trained')
+        return self
+
+    def predict(self, X, y=None):
+        """
+        Same as 'self.fit()', but call the 'predict()' method for each submodel and return the results.
+        """
+        groups = X.groupby(by=self.group_col)
+        results = []
+        for gp_key, group in groups:
+            x_group = group[self.selected_features[gp_key]]
+            gp_transformer = self.transformers[gp_key]
+            gp_model = self.sub_models.get(gp_key, DummyClassifier(constant=0).fit(x_group, [0] * len(x_group)))
+            # preprocessing
+            x_group = gp_transformer.transform(x_group)
+            # predict
+            result = gp_model.predict(x_group)
+            result = pd.Series(index=x_group.index, data=result)
+            results.append(result)
+        results = pd.concat(results, axis=0)
+        results = pd.Series(index=X.index, data=results, name='predicted')
+        return results
+
+    def initiliaze_models(self):
+        sub_models = {}
+        transformers = {}
+        for gp_key in self.clusters:
+            sub_models[gp_key] = xgboost.XGBRegressor(
+                n_estimators=self.params[gp_key].get('n_estimators', 100),
+                max_depth=self.params[gp_key].get('max_depth', 3),
+                subsample=self.params[gp_key].get('subsample', 1),
+                min_child_weight=self.params[gp_key].get('min_child_weight', 1),
+                gamma=self.params[gp_key].get('gamma', 0),
+                colsample_bytree=self.params[gp_key].get('colsample_bytree', 1),
+                learning_rate=self.params[gp_key].get('learning_rate', 0.1),
+                silent=True
+            )
+            transformer_name = self.params[gp_key].get('transformer', 'OneHotEncoder')
+            transformers.update({gp_key: copy.deepcopy(transformers.potential_transformers).get(transformer_name)})
+        return sub_models, transformers
+
+    @staticmethod
+    def set_categorical_features(data=None, potential_cat_feat=None):
+        # Declare categorical features for the transformer
+        # or leave the model find them automatically from the dataset.
+
+        if potential_cat_feat is None and isinstance(data, pd.DataFrame):
+            # declare automatically categorical features
+            potential_cat_feat = set(data.select_dtypes(include=['object']).columns)
+            potential_cat_feat.update([feat_name for feat_name, row in data.iteritems()
+                                       if 2 < len(row.unique()) < 30])
+        elif isinstance(potential_cat_feat, set):
+            potential_cat_feat = potential_cat_feat
+        else:
+            potential_cat_feat = None
+
+        return potential_cat_feat
---
+++ vf_portalytics/transformers.py
@@ -0,0 +1,64 @@
+import models.grouped_model
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class OneHotEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, categorical_features=None):
+        self.maps = dict()
+        self.categorical_features = categorical_features
+
+    def fit(self, X, y=None):
+        # find automatically categorical features if they are not previously declared
+        if self.categorical_features is None:
+            self.categorical_features = grouped_model.Grouped_Model.set_categorical_features(data=X, potential_cat_feat=None)
+
+        # Check columns are in X
+        for col in self.categorical_features:
+            if col not in X:
+                raise ValueError('Missing categorical feature: ' + col)
+
+        # Store each unique value for each cat feature
+        for col in self.categorical_features:
+            uniques = X[col].unique()
+            self.maps[col] = [unique for unique in uniques]
+
+        return self
+
+    def transform(self, X, y=None):
+
+        output = X.copy()
+        for col, vals in self.maps.items():
+            # apply label encoding
+            lookup = {x: i for i, x in enumerate(vals)}
+            missing = len(lookup)
+
+            output.loc[:, col] = output.loc[:, col].apply(lambda x: lookup.get(x, missing))
+            dummy_col = pd.get_dummies(X[col], prefix=col)
+            # Get missing columns
+            dummy_col = OneHotEncoder.get_missing_cols(dummy_col, col, lookup)
+
+            # drop the new category (the ones that were not in any category before)
+            # or even if in train set there is NaN delete it
+            dummy_col = dummy_col.drop(col + '_{}'.format(len(lookup)), axis=1, errors='ignore')
+            output = pd.concat([output, dummy_col], axis=1)
+
+            del output[col]
+        output = output.sort_index(axis=1)
+        return output
+
+    @staticmethod
+    def get_missing_cols(dummy_col, col, lookup):
+        # Get missing columns that were in the trainset but not in test
+        train_columns = [col + '_{}'.format(i) for i in lookup.keys()]
+        missing_cols = set(train_columns) - set(dummy_col.columns)
+        for c in missing_cols:
+            dummy_col[c] = 0
+        dummy_col = dummy_col[train_columns]
+        return dummy_col
+
+
+potential_transformers = {
+    'OneHotEncoder': OneHotEncoder()
+                          }
