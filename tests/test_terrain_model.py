f948705fc8ff790e6f13a1fc7c683c0887faf994
creukers <creukers@visualfabriq.com>
2024-04-25 15:54:51 +0200

    Update tests (WIP)

--- tests/test_terrain_model.py
+++ tests/test_terrain_model.py
@@ -5,70 +5,106 @@ import json
 import pandas as pd
 
 from vf_portalytics.terrain_model import Terrain
-from vf_portalytics.terrain_config import metadata
+import vf_portalytics.terrain_config as config
 from vf_portalytics.model import PredictionModel
+
+TEST_RANGE = 4
+TEST_ACCOUNTS = ['TEST_ACCOUNT_{}'.format(i) for i in range(TEST_RANGE)]
 
 
 def id_generator(size=24, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def generate_predict_df(n):
+product_index = [id_generator(6) for i in range(TEST_RANGE)]
+account_index = [id_generator(7) for i in range(TEST_RANGE)]
+prod_hash = [id_generator(5) for i in range(TEST_RANGE)]
+coef_index = pd.MultiIndex.from_product([TEST_ACCOUNTS, prod_hash], names=[config.customer_col, config.prod_line_col])
+
+coefs = pd.DataFrame({'discount_coef': [random.uniform(-1, -5) for i in range(len(coef_index))],
+                      'display_coef': [random.uniform(0.05, 0.30) for i in range(len(coef_index))],
+                      'feature_coef': [random.uniform(0.05, 0.30) for i in range(len(coef_index))],
+                      }, index=coef_index
+                     )
+
+account_id_map = pd.DataFrame({config.customer_col: TEST_ACCOUNTS,
+                               }, index=account_index
+                              )
+
+pid_map = pd.DataFrame({config.prod_line_col: prod_hash,
+                        }, index=product_index
+                       )
+
+
+def generate_predict_df(n=TEST_RANGE):
     df = pd.DataFrame({
-        'promo': [random.randrange(0, n / 2, 1) for i in range(n)],
-        'account_id': ['SIMPLIFIED'] * n,
-        'pid': [id_generator(6) for i in range(n)],
+        'promo': [random.randrange(0, round(n / 2), 1) for i in range(n)],
+        config.account_id_col: account_index,
+        'pid': product_index,
         'consumer_length': [random.randint(1, 5) for i in range(n)],
-        'baseline_units_ext': [random.randint(10, 10000) for i in range(n)],
+        config.baseline_col: [random.randint(10, 10000) for i in range(n)],
         'discount_perc': [random.randrange(10, 50, 10) for i in range(n)],
         'second_placement_perc': [random.choice([0, 50, 60, 70]) for i in range(n)],
         'feature_perc': [random.choice([0, 50]) for i in range(n)],
+        'promoted_price_ratio': [random.uniform(0.01, 0.5) for i in range(n)],
+        'on_display': [0.0] * n,
+        'on_feature': [0.0] * n
     })
 
     return df
 
 
 def generate_model_paths(model_data_path):
-    coefs.to_csv('{}/coefs.csv'.format(model_data_path))
-    account_id_map.to_csv('{}/account_id_map.csv'.format(model_data_path))
-    pid_map.to_csv('{}/pid_map.csv'.format(model_data_path))
+    coefs.to_parquet('{}/coefs.parquet'.format(model_data_path))
+    account_id_map.to_parquet('{}/account_id_map.parquet'.format(model_data_path))
+    pid_map.to_parquet('{}/pid_map.parquet'.format(model_data_path))
 
-    coefs_path = '{}/coefs.csv'.format(model_data_path)
-    account_id_mapper_path = '{}/account_id_map.csv'.format(model_data_path)
-    pid_mapper_path = '{}/pid_map.csv'.format(model_data_path)
+    coefs_path = '{}/coefs.parquet'.format(model_data_path)
+    account_id_mapper_path = '{}/account_id_map.parquet'.format(model_data_path)
+    pid_mapper_path = '{}/pid_map.parquet'.format(model_data_path)
 
     return coefs_path, account_id_mapper_path, pid_mapper_path
 
 
-coefs = pd.DataFrame({'discount_coef': [-1.1, -2.2, -3.3, -1.8, -2.1],
-                      'display_coef': [0.0, 0.0, 0.1, 0.13, 0.12],
-                      'feature_coef': [0.0, 0.0, 0.01, 0.11, 0.012],
-                      }, index=['SIMPLIFIED'] * 5
-                     )
+def test_terrain_with_prediction_model(tmpdir):
+    model_data_path = str(tmpdir.mkdir('model_data'))
+    coefs_path, account_id_mapper_path, pid_mapper_path = generate_model_paths(model_data_path)
 
-account_id_map = pd.DataFrame({'cpe_1': ['SIMPLIFIED'] * 5,
-                               }, index=[id_generator() for i in range(5)]
-                              )
+    terrain_model = Terrain(coefs_path=coefs_path, file_format='parquet', account_id_mapper_path=account_id_mapper_path,
+                            pid_mapper_path=pid_mapper_path)
+    model_id = 'terrain_test'
+    joblib.dump(terrain_model, '{0}/{1}.pkl'.format(model_data_path, model_id), compress=3)
+    with open('{0}/{1}.meta'.format(model_data_path, model_id), "w") as metadata_file:
+        json.dump(config.metadata, metadata_file)
 
-pid_map = pd.DataFrame({'prod_line_hash': [id_generator(10) for i in range(5)],
-                        }, index=[id_generator() for i in range(5)]
-                       )
+    model = PredictionModel(model_id, model_data_path)
+
+    df = generate_predict_df()
+    pred = model.predict(df)
+
+    assert not (pred == df[config.baseline_col]).all()
 
 
 def test_terrain_model_predict(tmpdir):
     model_data_path = str(tmpdir.mkdir('model_data'))
     coefs_path, account_id_mapper_path, pid_mapper_path = generate_model_paths(model_data_path)
 
-    terrain_model = Terrain(coefs_path=coefs_path, file_format='csv', account_id_mapper_path=account_id_mapper_path,
+    terrain_model = Terrain(coefs_path=coefs_path, file_format='parquet', account_id_mapper_path=account_id_mapper_path,
                             pid_mapper_path=pid_mapper_path)
-    model_id = 'terrain_test'
-    joblib.dump(terrain_model, '{0}/{1}.pkl'.format(model_data_path, model_id), compress=3)
-    with open('{0}/{1}.meta'.format(model_data_path, model_id), "w") as metadata_file:
-        json.dump(metadata, metadata_file)
+    df = generate_predict_df()
+    pred = terrain_model.predict(df)
 
-    model = PredictionModel(model_id, model_data_path)
+    assert not (pred == df[config.baseline_col]).all()
 
-    df = generate_predict_df(50)
-    pred = model.predict(df)
 
-    assert not (pred == 0).all()
+def test_get_factor(tmpdir):
+    model_data_path = str(tmpdir.mkdir('model_data'))
+    coefs_path, account_id_mapper_path, pid_mapper_path = generate_model_paths(model_data_path)
+
+    terrain_model = Terrain(coefs_path=coefs_path, file_format='parquet', account_id_mapper_path=account_id_mapper_path,
+                            pid_mapper_path=pid_mapper_path)
+
+    df = generate_predict_df()
+    factor = terrain_model._get_factor(df)
+
+    assert factor != 0
